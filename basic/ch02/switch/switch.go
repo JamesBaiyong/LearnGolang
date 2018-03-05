5423b2beb83f5a65b69a65b70ba268467e4d6580
mrbaiyongde@sina.com <gezipaopao@163.com>
2018-03-05 22:07:50 +0800

    switch update

--- basic/ch02/switch/switch.go
+++ basic/ch02/switch/switch.go
@@ -3,20 +3,39 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func os_know() {
-	switch os := runtime.GOOS; os {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS
+	os {
 	case "darwin":
 		fmt.Println("OS X.")
 	case "linux":
 		fmt.Println("Linux.")
 	default:
-		fmt.Printf("%s.",os)
+		fmt.Printf("deflult : %s.",os)
+	}
+}
+
+func when_saturday(){
+	fmt.Println("when is Saturday??")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Just Today.")
+	case today + 1:
+		fmt.Println("one day ago.")
+	case today + 2:
+		fmt.Println("two day ago.")
+	default:
+		fmt.Println("too far awy.")
 	}
 }
 
 func main() {
-	fmt.Print("Go runs on ")
 	os_know()
+	fmt.Println()
+	when_saturday()
 }
