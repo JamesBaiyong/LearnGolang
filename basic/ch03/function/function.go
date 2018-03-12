75a277608cda3a78dea0d76bd0a05883aa5f399e
JamesBaiyong <mrbaiyongde@sina.com>
2018-03-12 18:18:40 +0800

    闭包

--- basic/ch03/function/function.go
+++ basic/ch03/function/function.go
@@ -23,10 +23,50 @@ func (t *Int) add(i,j int) int {
 	return i + j
 }
 
+// 方法的闭包
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+// 斐波纳契闭包
+func fibonacci() func() int {
+	value1, value2 := 0, 1
+
+	return func() int {
+
+		temp := value1
+		
+		value1,value2 = value2 , (value1+value2)
+		
+		return temp
+	}
+}
+
+
 func main(){
-	var p Person
-	var t Int
+	var (
+		p Person
+		t Int
+	)
 	p.show("Jake",23)
 	p.show("James",24)
 	fmt.Println(t.add(3,3))
+
+	pos := adder()
+	neg := adder()
+	for i:=0;i<10;i++{
+		fmt.Println(
+			pos(i),
+			neg(-2 * i),
+		)
+	}
+
+	f := fibonacci()
+	for i:=0;i<10;i++{
+		fmt.Println(f())
+	}
 }
